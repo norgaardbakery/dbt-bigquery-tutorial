899bfbce2161a47a1435baf65ac5f7d06d0979b6
norgaardbakery@gmail.com <norgaardbakery@gmail.com>
2023-02-24 16:16:00 +0000

    Add dbt-utils package and update macros

--- models/orders_payment_method_amounts.sql
+++ models/orders_payment_method_amounts.sql
@@ -1,10 +1,13 @@
-{%- set payment_methods = get_payment_methods() -%}
+{%- set payment_methods = dbt_utils.get_column_values(
+    table=ref('raw_payments'),
+    column='payment_method'
+) -%}
 
 select
 order_id,
 {%- for payment_method in payment_methods %}
 sum(case when payment_method = '{{payment_method}}' then amount end) as {{payment_method}}_amount
-{{ "," if not loop.last }}
+{%- if not loop.last %},{% endif -%}
 {% endfor %}
 from {{ ref('raw_payments') }}
 group by 1
